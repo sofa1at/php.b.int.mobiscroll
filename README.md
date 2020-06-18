d22da286d7156d349160aa771f244beee75abe10
Christoph Zettl <c.zettl@sofa1.at>
2020-06-18 16:28:32 +0200

    changed Converter Methods

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ This repository holds all interfaces/classes/traits related to PSR-3.
 
 composer require sofa1at/mobiscrollinterpreter
 
+````php
+$converter = new Sofa1MobiscrollConverter();
+
+// add businesshours
+$converter->AddBusinessHours($mockDataBusinessHours);
+
+// add timesettings
+$converter->AddTimeSetting($mockTimeSetting);
+
+// receive json
+$converter->ToString();
+````
--- Sofa1/Mobiscroll/Sofa1MobiscrollConverter.php
+++ Sofa1/Mobiscroll/Sofa1MobiscrollConverter.php
@@ -4,31 +4,102 @@
 namespace Sofa1\Mobiscroll;
 
 use DateInterval;
-use DatePeriod;
 use DateTime;
-use Exception;
 use Sofa1\Mobiscroll\Helpers\DateTimeHelper;
 use Sofa1\Mobiscroll\Models\AbstractBusinessHours;
 use Sofa1\Mobiscroll\Models\AbstractTimeSetting;
+use Sofa1\Mobiscroll\Models\AbstractTimeSettingPeriod;
+use Sofa1\Mobiscroll\Models\AbstractTimeSettingPeriodDay;
 use Sofa1\Mobiscroll\Models\WeekDayElement;
 use Sofa1\Mobiscroll\Models\WeekdayTimeRangeElement;
 
 class Sofa1MobiscrollConverter
 {
+
 	/**
-	 * @param $businessHours AbstractBusinessHours[]
-	 *
+	 * @var DateTimeValidation
+	 */
+	private $validation;
+
+	/**
+	 * @var AbstractBusinessHours[]
+	 */
+	private $businessHours;
+
+	/**
+	 * @var AbstractTimeSetting[]
+	 */
+	private $timeSettings;
+
+	/**
+	 * @var int $max
+	 */
+	private $max;
+
+	/**
+	 * @var DateTime
+	 */
+	private $startDate;
+
+	public function __construct($max = 365, $startDate = null)
+	{
+		$this->businessHours = array();
+		$this->timeSettings = array();
+		$this->max = $max;
+		if ($startDate == null)
+		{
+			$this->startDate = new DateTime('now', new \DateTimeZone('UTC'));
+		}
+	}
+
+	/**
+	 * @param AbstractBusinessHours[] $businessHours
+	 */
+	public function AddBusinessHours($businessHours)
+	{
+		$this->businessHours = array_merge($this->businessHours, $businessHours);
+	}
+
+	/**
+	 * @param AbstractTimeSetting $timeSettings
+	 */
+	public function AddTimeSettings($timeSettings)
+	{
+		$this->timeSettings[] = $timeSettings;
+	}
+
+	/**
 	 * @return string
 	 */
-	public function GetValidTimesByBusinessHoursJson($businessHours)
+	public function ToString()
 	{
-		if (empty($businessHours))
+		$this->validation = new DateTimeValidation();
+		if ( ! empty($this->businessHours) && empty($this->timeSettings))
 		{
-			return "";
+			$this->ValidateBusinessHours();
+		}
+		else if ( ! empty($this->timeSettings) && empty($this->businessHours))
+		{
+			$this->ValidateTimeSettings();
+		}
+		else
+		{
+			$this->AddTimeSettings($this->ConvertBusinessHoursToTimeSetting());
+			$this->ValidateTimeSettings();
 		}
 
-		$sortedBusinessHours = $this->GetBusinessHoursSorted($businessHours, 'sunday'); // sunday is needed for the mobiscroll control offset
-		$validation = new DateTimeValidation();
+		return $this->validation->ToString();
+	}
+
+
+	public function ValidateBusinessHours()
+	{
+		if (empty($this->businessHours))
+		{
+			return null;
+		}
+
+		$sortedBusinessHours = $this->GetBusinessHoursSorted($this->businessHours, 'sunday'); // sunday is needed for the mobiscroll control offset
 		for ($i = 0; $i <= 6; $i ++)
 		{
 			if ( ! empty($sortedBusinessHours[$i]) && count($sortedBusinessHours[$i]) > 0)
@@ -50,100 +121,99 @@ class Sofa1MobiscrollConverter
 
 				if ( ! $hasClosedElement && ! empty($valideElements))
 				{
-					$validation->Invalid->AddWeekday($i);
-					$validation->Valid->AddArray($valideElements);
+					$this->validation->Invalid->AddWeekday($i);
+					$this->validation->Valid->AddArray($valideElements);
 				}
 				else
 				{
-					$validation->Invalid->Add(new WeekDayElement($i, DateOutputMethod::EveryWeekDay));
+					$this->validation->Invalid->Add(new WeekDayElement($i, DateOutputMethod::EveryWeekDay));
 				}
 			}
 			else
 			{
-				$validation->Invalid->Add(new WeekDayElement($i, DateOutputMethod::EveryWeekDay));
+				$this->validation->Invalid->Add(new WeekDayElement($i, DateOutputMethod::EveryWeekDay));
 			}
 		}
+	}
 
-		return $validation->ToString();
+	public function ValidateTimeSettings()
+	{
+		if (empty($this->timeSettings))
+		{
+			return;
+		}
+
+		for ($i = 0; $i < 7; $i ++)
+		{
+			$this->validation->Invalid->Add(new WeekDayElement($i, DateOutputMethod::EveryWeekDay));
+		}
+
+		foreach ($this->timeSettings as $timeSetting)
+		{
+			$date = clone $this->startDate;
+			for ($i = 0; $i <= $this->max; $i ++)
+			{
+				foreach ($timeSetting->TimeSettingPeriods as $timeSettingPeriod)
+				{
+					$fromDate = $timeSettingPeriod->FromDate->format("md");
+					$toDate = $timeSettingPeriod->ToDate->format("md");
+					if ($date->format("md") < $fromDate || $date->format("md") > $toDate)
+					{
+						continue;
+					}
+					if ( ! empty($timeSettingPeriod->TimeSettingPeriodDays))
+					{
+						foreach ($timeSettingPeriod->TimeSettingPeriodDays as $periodDay)
+						{
+							$dayofweek = $date->format('l');
+							if ((empty($periodDay->Day) ? true : $periodDay->Day == DateTimeHelper::GetWeekdayNumberFromString($dayofweek)))
+							{
+								$this->validation->AddValidDateTimeRange(clone $date, $periodDay->FromTime->format("H:i"), $periodDay->ToTime->format("H:i"));
+							}
+						}
+					}
+					else
+					{
+						$this->validation->AddValidDateTimeRange(clone $date, "00:00", "23:59");
+					}
+				}
+
+				$date->add(new DateInterval('P1D'));
+			}
+		}
 	}
 
 	/**
-	 * @param $timeSetting AbstractTimeSetting
-	 *
-	 * @param $startDate DateTime
-	 * @param $maxDays int
-	 *
-	 * @return string
-	 * @throws Exception
+	 * @return AbstractTimeSetting|null
+	 * @throws \Exception
 	 */
-	public function GetValidTimesByTimeSettingsJson($timeSetting, $startDate, $maxDays)
+	private function ConvertBusinessHoursToTimeSetting()
 	{
-		/**
-		 * excepted result
-		 * invalid: [
-		 * 'w2', // Every Tuesday
-		 * { d: new Date(2020, 5, 23), start: '00:00', end: '23:59' } // Specific Tuesday invalid times
-		 * ],
-		 * valid: [
-		 * '2020-06-23', // Enable specific Tuesday
-		 * { d: 'new Date(2020, 5, 23),' start: '12:00', end: '21:59' } // Specific Tuesday valid times
-		 * ]
-		 */
-
-		if (empty($timeSetting) || empty($timeSetting->TimeSettingPeriods))
+		if (empty($this->businessHours))
 		{
-			return "";
+			return null;
 		}
 
-		$validation = new DateTimeValidation();
-		for ($i = 0; $i < 7; $i ++)
+		$startDate = clone $this->startDate;
+		$endDate = clone $this->startDate;
+		$endDate->add(new DateInterval('P' . $this->max . 'D'));
+
+		$timeSetting = new AbstractTimeSetting();
+		$timeSettingPeriod = new AbstractTimeSettingPeriod();
+		$timeSettingPeriod->FromDate = $startDate;
+		$timeSettingPeriod->ToDate = $endDate;
+
+		foreach ($this->businessHours as $businessHour)
 		{
-			$validation->Invalid->Add(new WeekDayElement($i, DateOutputMethod::EveryWeekDay));
+			$periodDay = new AbstractTimeSettingPeriodDay();
+			$periodDay->Day = $businessHour->Day;
+			$periodDay->FromTime = new DateTime(sprintf("1990-01-01 %s", $businessHour->From));
+			$periodDay->ToTime = new DateTime(sprintf("1990-01-01 %s", $businessHour->To));
+			$timeSettingPeriod->TimeSettingPeriodDays[] = $periodDay;
 		}
+		$timeSetting->TimeSettingPeriods[] = $timeSettingPeriod;
 
-		$date = clone $startDate;
-		for ($i = 0; $i <= $maxDays; $i ++)
-		{
-			foreach ($timeSetting->TimeSettingPeriods as $timeSettingPeriod)
-			{
-				$fromDate = $timeSettingPeriod->FromDate->format("md");
-				$toDate = $timeSettingPeriod->ToDate->format("md");
-				if ($date->format("md") < $fromDate || $date->format("md") > $toDate)
-				{
-					continue;
-				}
-				if ( ! empty($timeSettingPeriod->TimeSettingPeriodDays))
-				{
-					foreach ($timeSettingPeriod->TimeSettingPeriodDays as $periodDay)
-					{
-						$dayofweek = $date->format('l');
-						if ((empty($periodDay->Day) ? true : $periodDay->Day == DateTimeHelper::GetWeekdayNumberFromString($dayofweek)))
-						{
-							$validation->AddValidDateTimeRange(clone $date, $periodDay->FromTime->format("H:i"), $periodDay->ToTime->format("H:i"));
-						}
-					}
-				}
-				else
-				{
-					$validation->AddValidDateTimeRange(clone $date, "00:00", "23:59");
-				}
-			}
-
-			$date->add(new DateInterval('P1D'));
-		}
-
-		// format timesettings to array
-		// '06-05' =>
-		//    array (size=1)
-		//      0 =>
-		//        object(App\TimeSettings\TimeSettingPeriodDay)[70]
-		//  '07-05' =>
-		//    array (size=1)
-		//      0 =>
-		//        object(App\TimeSettings\TimeSettingPeriodDay)[70]
-
-
-		return $validation->ToString();
+		return $timeSetting;
 	}
 
 	private function GetBusinessHoursSorted($businessHours, $firstDayOfWeek = 'monday')
